563303317c99a3cee102b485cae14aaa2d1aff01
yahaa <1477765176@qq.com>
2016-03-27 15:50:03 +0800

    Thread

--- Threads/PrintNum.java
+++ Threads/PrintNum.java
@@ -8,15 +8,9 @@ public class PrintNum implements Runnable {
 	}
 	
 	public void run(){
-		try{
-			for(int i=0;i<times;i++){
-				System.out.print(number);
-				if(i%10==0)System.out.println();
-				Thread.sleep(100);
-			}
-		}
-		catch(InterruptedException ex){
-			ex.printStackTrace();
+		for(int i=0;i<times;i++){
+			System.out.print(number);
+			if(i%10==0)System.out.println();
 		}
 		
 	}
--- Threads/TaskThread.java
+++ Threads/TaskThread.java
@@ -1,25 +1,15 @@
 import java.util.*;
+import java.util.concurrent.*;
 public class TaskThread {
 	public static void main(String[]args){
-//		Runnable a=new PrintChar('a',100);
-//		Runnable b=new PrintChar('b',100);
-//		Runnable c=new PrintNum(12,100);
-//		Thread thread1=new Thread(a);
-//		Thread thread2=new Thread(b);
-//		Thread thread3=new Thread(c);
-//		thread1.start();
-//		thread2.start();
-//		thread3.start();
-//		thread3.setPriority(Thread.MAX_PRIORITY);
-//		System.out.println(thread1.getPriority());
-//		System.out.println(thread2.getPriority());
-//		System.out.println(thread3.getPriority());
-//		System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxx");
-		FlashingText a=new FlashingText();
-		Thread thread4=new Thread(a);
-		thread4.start();
-		
-		
+		ExecutorService executor=Executors.newCachedThreadPool();
+		executor.execute(new PrintChar('q',100));
+		executor.execute(new PrintChar('y',100));
+		executor.execute(new PrintChar('e',100));
+		executor.execute(new PrintNum(565,1000));
+		executor.execute(new PrintNum(199,1000));
+		executor.execute(new PrintNum(1234,1000));
+		executor.shutdown();
 	}
 
 }
